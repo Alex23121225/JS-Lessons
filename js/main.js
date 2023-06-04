fcaa9eb364a23f2e9d31a15843d27d7025c463d6
Alex <alex-shuv@mail.ru>
2023-06-04 16:03:50 +0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: true
+};
+
+const a = prompt('Один из последних посмотренных фильмов ?', ''),
+  b = prompt('На сколько вы его оцените ?', ''),
+  c = prompt('Один из последних посмотренных фильмов ?', ''),
+  d = prompt('На сколько вы его оцените ?', '');
+personalMoviesDB.movies[a] = b;
+personalMoviesDB.movies[c] = d;
+
+console.log(personalMoviesDB);
